f8288826ac2743a0b396e757d0d74be54a725bf1
thinh2yuhjd <thinhmai999@gmail.com>
2024-10-25 17:15:17 +0700

    aa

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -22,22 +22,22 @@ const Header = () => {
               <div className="col-md-8">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-evenly align-items-center">
                   <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">
+                    <a className="nav-link active color_nav" aria-current="page" href="#" >
                       Trang chủ
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">
+                    <a className="nav-link active color_nav" aria-current="page" href="#">
                       Sản phẩm
                     </a>
                   </li>
                   <li className="nav-item">
-                    <Link to="/doc" className="nav-link active" aria-current="page">
+                    <Link to="/doc" className="nav-link active color_nav" aria-current="page">
                       Tài liệu
                     </Link>
                   </li>
                   <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false" onClick={()=>setOpen(p=>!p)}>
+                    <a className="nav-link dropdown-toggle color_nav" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false" onClick={()=>setOpen(p=>!p)}>
                       Mua ngay
                     </a>
                     {
@@ -51,8 +51,8 @@ const Header = () => {
                           display: "unset",
                            background: ""
                         }}>
-                          <li><a className="dropdown-item" href="#">Action</a></li>
-                          <li><a className="dropdown-item" href="#">Another action</a></li>
+                          <li><a className="dropdown-item color_nav" href="#">Action</a></li>
+                          <li><a className="dropdown-item color_nav" href="#">Another action</a></li>
                         </ul>
                       )
                     }
@@ -61,7 +61,7 @@ const Header = () => {
                 </ul>
               </div>
               <div className="col-md-4 d-flex justify-content-start">
-                <form className="d-flex align-items-center w-100 justify-content-start">
+                {/* <form className="d-flex align-items-center w-100 justify-content-start">
                   <input
                     className="form-control me-2"
                     type="search"
@@ -71,8 +71,8 @@ const Header = () => {
                   <button className="btn btn-outline-custom" type="submit">
                     Search
                   </button>
-                </form>
-                <button onClick={onhangdleLogin}>Đăng nhập</button>
+                </form> */}
+                <button className="btn border btn-outline-custom" onClick={onhangdleLogin}>Đăng nhập</button>
               </div>
             </div>
           </div>
