7c98b25ea1ed8f5a72dfd51c08da09e71ce9e480
chinhhoangtc <161810260+chinhhoangtc@users.noreply.github.com>
2024-10-26 15:42:06 +0700

    commit done buy license

--- src/page/license/content/buylicense/buylicense.jsx
+++ src/page/license/content/buylicense/buylicense.jsx
@@ -1,12 +1,129 @@
-import React from 'react'
-import './index.css'
+import React, { useState } from 'react';
+import './index.css';
+import { dataLicense } from './data';
+
+
+import iconUp from '../../../../img/icon/arrowhead-up.png';
+import iconDown from '../../../../img/icon/arrowhead-down.png';
+
+function CustomSelect({ options, onChange, value }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => setIsOpen(!isOpen);
+  const handleOptionClick = (option) => {
+    onChange(option);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="custom-dropdown">
+      <div onClick={toggleDropdown} className="custom-dropdown-selected d-flex justify-content-between">
+        {value?.name || 'Chọn gói phần mềm'}
+        <div>
+          <img src={isOpen ? iconDown : iconUp} alt="Toggle Dropdown" />
+        </div>
+      </div>
+      {isOpen && (
+        <div className="custom-dropdown-options">
+          {options.map((option, index) => (
+            <div key={index} onClick={() => handleOptionClick(option)} className="custom-dropdown-option">
+              {option.name} (vĩnh viễn)
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
 export function BuyLicensePage() {
+  const [selectedOption, setSelectedOption] = useState(dataLicense[0]);
+  const [isModuleChecked, setIsModuleChecked] = useState(false);
+  const modulePrice = 2000000;
+
+  const handleLicenseChange = (option) => {
+    setSelectedOption(option);
+  };
+
+  const handleModuleChange = (event) => {
+    setIsModuleChecked(event.target.checked);
+  };
+
+  const totalPrice = selectedOption.price + (isModuleChecked ? modulePrice : 0);
+
   return (
     <div className='container pt-5'>
-      <h3 style={{ fontWeight: '600' }}>Mua license</h3>
-      <div className="mt-4 bg-white border p-4 rounded">
-        <h3 className='title'>Thông tin phần mềm</h3>
+      <h3 className="fw-bold mb-4">Mua license</h3>
+      <div className="p-4 bg-white border rounded shadow-sm">
+        <h4 className="mb-4 title">Thông tin phần mềm</h4>
+        <div className="row g-3">
+          <div className="col-md-6">
+            <label htmlFor="licensePackage" className="form-label">Gói phần mềm</label>
+            <CustomSelect
+              options={dataLicense}
+              value={selectedOption}
+              onChange={handleLicenseChange}
+            />
+          </div>
+          <div className="col-md-6">
+            <span >Module</span>
+            <div className=" mt-2 rounded border" style={{ backgroundColor: '#f7f7f7', padding: '10px' }}>
+              <input type="checkbox" id="module" className="form-check-input" onChange={handleModuleChange} />
+              <label className="form-check-label ms-2" htmlFor="module">Private server</label>
+            </div>
+          </div>
+        </div>
+        <div className="mt-4">
+          <strong>Tổng tiền: {totalPrice.toLocaleString()} VND</strong>
+        </div>
+      </div>
+      <div className="p-4 bg-white border rounded shadow-sm mt-4">
+        <h4 className="mb-4 title">Thông tin khách hàng</h4>
+        <p className='sub-title'>Thông tin khách hàng</p>
+        <div className="row">
+          <div className="col-md-6">
+            <span >Họ và tên <span style={{ color: 'red' }}>*</span></span>
+            <div className="mt-2 w-100 border">
+              <input className='border-0 rounded' type="text" style={{ width: '100%', padding: '10px' }} />
+            </div>
+          </div>
+          <div className="col-md-6">
+            <span >Số điện thoại<span style={{ color: 'red' }}>*</span></span>
+            <div className="mt-2 w-100 border">
+              <input className='border-0 rounded' type="text" style={{ width: '100%', padding: '10px' }} />
+            </div>
+          </div>
+        </div>
+        <div className="row mt-4">
+          <div className="col-md-4">
+            <label className='mb-2' htmlFor="">Tỉnh / Thành phố <span style={{ color: 'red' }}>*</span></label>
+            <CustomSelect
+              options={dataLicense}
+              value={selectedOption}
+              onChange={handleLicenseChange}
+            />
+          </div>
+          <div className="col-md-4">
+            <label className='mb-2' htmlFor="">Huyện <span style={{ color: 'red' }}>*</span></label>
+
+            <CustomSelect
+              options={dataLicense}
+              value={selectedOption}
+              onChange={handleLicenseChange}
+            />
+          </div>
+          <div className="col-md-4">
+            <label className='mb-2' htmlFor="">Xã / phường <span style={{ color: 'red' }}>*</span></label>
+
+            <CustomSelect
+              options={dataLicense}
+              value={selectedOption}
+              onChange={handleLicenseChange}
+            />
+          </div>
+        </div>
+
+
       </div>
     </div>
-  )
+  );
 }
--- src/page/license/licensemanagement.jsx
+++ src/page/license/licensemanagement.jsx
@@ -1,24 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate, useLocation } from 'react-router-dom';
 
 import iconMenubar from '../../img/icon/menu.png';
 import iconLogo from '../../img/logo.png';
 import inconLogdai from '../../img/logodai.png';
-import iconVienam from '../../img/icon/vietnam.png'
+import iconVienam from '../../img/icon/vietnam.png';
 import iconUser from '../../img/icon/user.png';
 import iconDashboard from '../../img/icon/dashboard.png';
 import iconLicense from '../../img/icon/cart.png';
 import iconLicenseManagement from '../../img/icon/management.png';
 import iconSupport from '../../img/icon/customer-service.png';
 
-
-
-
 import { Link } from 'react-router-dom';
-export function LicenseManagement() {
 
+export function LicenseManagement() {
     const [activeIndex, setActiveIndex] = useState(0);
+    const navigate = useNavigate();
+    const location = useLocation();
 
     const menuItems = [
         { icon: iconDashboard, text: 'Tổng quan', path: '/license/dashboard' },
@@ -26,62 +25,57 @@ export function LicenseManagement() {
         { icon: iconLicenseManagement, text: 'Quản lý license', path: '/license/management' },
         { icon: iconSupport, text: 'Hỗ trợ', path: '/license/support' }
     ];
-
     const [isCollapsed, setIsCollapsed] = useState(false);
 
     const toggleSidebar = () => {
         setIsCollapsed(!isCollapsed);
     };
 
+    useEffect(() => {
+        if (location.pathname === '/license') {
+            navigate('/license/dashboard');
+        }
+    }, [location.pathname, navigate]);
+
     return (
-        <>
-            <div className="d-flex h-full">
-
-                <div className="siderbar_page h-100 d-flex ">
-                    <div className={` sub_siderbar border-end ${isCollapsed ? 'collapsed' : 'expanded'}`}>
-                        <div className="sidebar_header border-bottom ">
-                            {!isCollapsed ? (<img src={inconLogdai} className='logo' />) : (<img src={iconLogo} className='logo' />)}
-                        </div>
-                        <div className="">
-                            <ul className="list-unstyled custom-ul">
-                                {menuItems.map((item, index) => (
-                                    <Link to={item.path}>
-                                        <li key={index} className={`custom-li ${activeIndex === index ? 'active' : ''}`} onClick={() => setActiveIndex(index)}>
-                                            <img src={item.icon} width="25px" className="me-4" alt="" />
-                                            {!isCollapsed && <span>{item.text}</span>}
-                                        </li>
-                                    </Link>
-                                ))}
-                            </ul>
-                        </div>
+        <div className="d-flex h-full">
+            <div className="siderbar_page h-100 d-flex ">
+                <div className={`sub_siderbar border-end ${isCollapsed ? 'collapsed' : 'expanded'}`}>
+                    <div className="sidebar_header border-bottom">
+                        {!isCollapsed ? (<img src={inconLogdai} className='logo' alt="Logo" />) : (<img src={iconLogo} className='logo' alt="Logo" />)}
                     </div>
-                </div>
-                <div className="content_page w-100">
-                    <div className="d-flex justify-content-between align-items-center w-100 content_header border-bottom">
-
-                        <div className="">
-                            <button className='button_menubar border-0 ms-2' onClick={toggleSidebar}>
-                                <img width='28px' src={iconMenubar} alt="" />
-                            </button>
-                        </div>
-                        <div className="d-flex me-4 gap-2">
-                            <div className="">
-                                <button className='button_menubar border-0'>
-                                    <img width='32px' src={iconVienam} alt="" />
-                                </button>
-                            </div>
-                            <div className="">
-                                <button className='button_menubar border-0'>
-                                    <img width='32px' src={iconUser} alt="" />
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="sub_content h-100">
-                        <Outlet />
+                    <div>
+                        <ul className="list-unstyled custom-ul">
+                            {menuItems.map((item, index) => (
+                                <Link to={item.path} key={index}>
+                                    <li className={`custom-li ${activeIndex === index ? 'active' : ''}`} onClick={() => setActiveIndex(index)}>
+                                        <img src={item.icon} width="25px" className="me-4" alt="" />
+                                        {!isCollapsed && <span>{item.text}</span>}
+                                    </li>
+                                </Link>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             </div>
-        </>
+            <div className="content_page w-100">
+                <div className="d-flex justify-content-between align-items-center w-100 content_header border-bottom">
+                    <button className='button_menubar border-0 ms-2' onClick={toggleSidebar}>
+                        <img width='28px' src={iconMenubar} alt="Toggle Sidebar" />
+                    </button>
+                    <div className="d-flex me-4 gap-2">
+                        <button className='button_menubar border-0'>
+                            <img width='32px' src={iconVienam} alt="Vietnam" />
+                        </button>
+                        <button className='button_menubar border-0'>
+                            <img width='32px' src={iconUser} alt="User" />
+                        </button>
+                    </div>
+                </div>
+                <div className="sub_content h-100">
+                    <Outlet />
+                </div>
+            </div>
+        </div>
     );
 }
