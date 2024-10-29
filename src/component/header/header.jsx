34d9946daa664cd414e26822e1470f0046b48fe3
thinh2yuhjd <thinhmai999@gmail.com>
2024-10-29 10:59:17 +0700

    update

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 import logo from "../../img/logo.png";
-import './index.css';
-import { useNavigate, Link } from 'react-router-dom';
+import "./index.css";
+import { useNavigate, Link } from "react-router-dom";
 
 const Header = () => {
   const navigate = useNavigate();
   const onhangdleLogin = () => {
-    navigate('/login');
-  }
+    navigate("/login");
+  };
 
-  const[open,setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   return (
-    <div className="container-fluid sticky-top" style={{backgroundColor:'white'}}>
+    <div
+      className="container-fluid sticky-top"
+      style={{ backgroundColor: "white" }}
+    >
       <div className="row pt-3">
         <nav className="navbar navbar-expand-lg navbar-light">
           <div className="col-md-2 col-sx-12 d-flex justify-content-end align-items-center">
@@ -22,41 +25,67 @@ const Header = () => {
               <div className="col-md-8">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-evenly align-items-center">
                   <li className="nav-item">
-                    <a className="nav-link active color_nav" aria-current="page" href="#" >
+                    <a
+                      className="nav-link active color_nav"
+                      aria-current="page"
+                      href="#"
+                    >
                       Trang chủ
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link active color_nav" aria-current="page" href="#">
+                    <a
+                      className="nav-link active color_nav"
+                      aria-current="page"
+                      href="#"
+                    >
                       Mua bán tài nguyên
                     </a>
                   </li>
                   <li className="nav-item">
-                    <Link to="/doc" className="nav-link active color_nav" aria-current="page">
+                    <Link
+                      to="/doc"
+                      className="nav-link active color_nav"
+                      aria-current="page"
+                    >
                       Tài liệu
                     </Link>
                   </li>
                   <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle color_nav" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false" onClick={()=>setOpen(p=>!p)}>
+                    <a
+                      className="nav-link dropdown-toggle color_nav"
+                      href="#"
+                      id="navbarDropdownMenuLink"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                      onClick={() => setOpen((p) => !p)}
+                    >
                       Mua ngay
                     </a>
-                    {
-                      open && (
-                        <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink" style={{
+                    {open && (
+                      <ul
+                        className="dropdown-menu"
+                        aria-labelledby="navbarDropdownMenuLink"
+                        style={{
                           position: "absolute",
                           top: "100%",
                           left: 0,
-                     
-                          
+
                           display: "unset",
-                           background: ""
-                        }}>
-                          <li><a className="dropdown-item color_nav" href="#">Mua qua facebook</a></li>
-                          <li><a className="dropdown-item color_nav" href="#">Mua online</a></li>
-                        </ul>
-                      )
-                    }
-                    
+                          background: "",
+                        }}
+                      >
+                        <li>
+                          <a className="dropdown-item color_nav" href="#">
+                            Mua qua facebook
+                          </a>
+                        </li>
+                        <li className="dropdown-item color_nav">
+                          <Link to="/license" className="text-decoration-none color_nav"> Mua online</Link>
+                        </li>
+                      </ul>
+                    )}
                   </li>
                 </ul>
               </div>
@@ -72,7 +101,12 @@ const Header = () => {
                     Search
                   </button>
                 </form> */}
-                <button className="btn border btn-outline-custom" onClick={onhangdleLogin}>Đăng nhập</button>
+                <button
+                  className="btn border btn-outline-custom"
+                  onClick={onhangdleLogin}
+                >
+                  Đăng nhập
+                </button>
               </div>
             </div>
           </div>
