a8f37a7704068f3877e0763826c14bb47053389b
chinhhoangtc <161810260+chinhhoangtc@users.noreply.github.com>
2024-11-01 14:51:22 +0700

    cmt

--- src/page/test/test.jsx
+++ src/page/test/test.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './test.css'
 export default function Test() {
-  return (
-    <div className='container-fluid'>
-        <div className="headera d-flex">
-            <div className="headers border " style={{width:'350px'}}>
-                <div className="border h-100" style={{width:'250px'}}></div>
-                <button>aaa</button>
-            </div>
-            <div className="headerc border" style={{width:'100%'}}>headerc</div>
 
+  const [isopenSidebar, setisopenSidebar] = useState(false);
+  const handleOpen = () => {
+    setisopenSidebar(!isopenSidebar)
+  }
+  return (
+    <div className=''>
+      <div className="header-main d-flex border">
+        <div className={`header-sidebar ${isopenSidebar ? 'collaps' : 'expends'} d-flex position-relative`}>
+          <div className={`border sidebar-sub ${isopenSidebar ? 'collaps' : 'expends'}`}>icon</div>
+          <button className='position-absolute icon-sub' onClick={handleOpen}>aaa</button>
         </div>
-        <div className="d-flex">
-            <div className="contents" style={{width:'350px'}}>contents</div>
-            <div className="contentc" style={{width:'100%'}}>contentc</div>
-        </div>
+        <div className="header-content text-end w-100"></div>
+
+      </div>
+      <div className="d-flex">
+        <div className={`content-sidebar ${isopenSidebar ? 'collaps' : 'expends'} border`}>contents</div>
+        <div className="content-content border">contentc</div>
+      </div>
     </div>
   )
 }
