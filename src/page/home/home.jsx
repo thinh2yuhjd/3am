da45c478edb17f919614320c1ca015e5927ede4c
chinhhoangtc <161810260+chinhhoangtc@users.noreply.github.com>
2024-11-02 16:31:47 +0700

    aaa

--- src/page/home/home.jsx
+++ src/page/home/home.jsx
@@ -12,8 +12,9 @@ import why3am from "../../img/why3am.png";
 import Video from "./video";
 import "./index.css";
 import background from "../../img/bg.png";
-
+import button_search from "../../img/icon/Button.png";
 import { useNavigate, Link } from "react-router-dom";
+
 const Home = () => {
   const navigate = useNavigate();
   const onhangdleLogin = () => {
@@ -43,20 +44,20 @@ const Home = () => {
   }, []);
 
   return (
-    <div style={{ backgroundColor: "#F4F6F8", overflow: "scroll" }}>
+    <div style={{ backgroundColor: "#F4F6F8" }}>
       <div className="am-header">
-        <img src={background} width={'100%'}/>
+        <img src={background} width={'100%'} />
         <div className="am-header-search">
           <div
             className="border rounded-pill"
-            style={{ width: "100%", backgroundColor: "#FFFFFF"}}
+            style={{ width: "100%", backgroundColor: "#FFFFFF" }}
           >
-            <div className="row " style={{ height: "64px" }}>
-              <div className="col-md-3 my-auto text-start">
+            <div className="row" style={{ height: "64px" }}>
+              <div className="col-3 my-auto text-start">
                 <img src={logo} alt="Logo" className="ps-3" />
               </div>
-              <div className="col-md-6 my-auto">
-                <ul className="d-flex">
+              <div className="col-6 my-auto">
+                <ul className="d-flex justify-content-around flex-wrap">
                   <li className="nav-item">
                     <a className="nav-link active color_nav" href="#">
                       Trang Chủ
@@ -106,7 +107,7 @@ const Home = () => {
                   </li>
                 </ul>
               </div>
-              <div className="col-md-3 my-auto d-flex justify-content-end pe-4">
+              <div className="col-3 my-auto d-flex justify-content-end pe-4">
                 <div onClick={handleOpen}>
                   <img src={search_icon} alt="search" className="pe-3" />
                 </div>
@@ -115,7 +116,7 @@ const Home = () => {
                   style={{ width: "115px", backgroundColor: "#1C252E" }}
                   onClick={onhangdleLogin}
                 >
-                  <a style={{ color: "white" }}>Đăng ký</a>
+                  <span style={{ color: "white" }}>Đăng ký</span>
                 </button>
               </div>
             </div>
@@ -123,24 +124,24 @@ const Home = () => {
           {isOpenSearch && (
             <div className="search-popup-overlay" onClick={handleOpen}>
               <div
-                className="search-popup-content"
+                className="search-popup-content font-border"
                 onClick={(e) => e.stopPropagation()}
               >
-                <h2>Khu vực tìm kiếm</h2>
                 <input
                   type="text"
-                  placeholder="Search..."
-                  className="form-control"
+                  placeholder="Tìm kiếm..."
+                  className="form-controls outline-none"
+                  style={{ outline: 'none' }}
                 />
-                <button className="btn btn-primary mt-2">Tìm kiếm</button>
+                <img src={button_search} alt="" />
               </div>
             </div>
           )}
         </div>
         <div className="am-header-content mt-1">
-          <div className="container pt-5 text-start">
-            <div className="col-md-3" style={{ color: "white" }}>
-              <h1 style={{ fontSize: "60px", fontWeight: "600" }}>
+          <div className="container text-start">
+            <div className="col-md-9 col-lg-6 col-sm-12" style={{ color: "white" }}>
+              <h1 className="fs-1 fs-md-2 fs-lg-3 text-white" style={{ fontWeight: "600" }}>
                 Web3 Automation Multiple
               </h1>
               <p className="last-updated pt-3">
@@ -148,32 +149,32 @@ const Home = () => {
                 trên một máy tính giúp bảo vệ danh tính, vận hành an toàn và
                 hiệu quả cho các hoạt động tiếp thị và quản lý tài khoản.
               </p>
-              <div className="">
-                <button
-                  className="button d-flex align-items-center justify-content-evenly p-2"
-                  style={{ width: "250px", backgroundColor: "#24A1DE" }}
-                >
-                  <span className="icon "></span>
-                  <a
-                    href="#"
-                    className="text-decoration-none"
-                    style={{ color: "white" }}
-                  >
-                    Tải xuống cho Windows
-                  </a>
-                </button>
-              </div>
+              
+<div className="">
+  <button
+    className="button d-none d-lg-flex align-items-center justify-content-evenly p-2" // Hiện trên máy tính lớn, ẩn trên thiết bị nhỏ hơn
+    style={{ width: "250px", backgroundColor: "#24A1DE" }}
+  >
+    <span className="icon"></span>
+    <a
+      href="#"
+      className="text-decoration-none"
+      style={{ color: "white" }}
+    >
+      Tải xuống cho Windows
+    </a>
+  </button>
+</div>
             </div>
-            <div className="col-md-9"></div>
           </div>
         </div>
       </div>
 
       <div className="">
-        <h1 style={{ fontSize: "50px", fontWeight: "600", color: "#172755" }}>
-          Được hơn 20.000 người dùng tin tưởng.
-        </h1>
-      </div>
+  <h1 className="custom-title" style={{ fontWeight: "600", color: "#172755" }}>
+    Được hơn 20.000 người dùng tin tưởng.
+  </h1>
+</div>
       <div
         className="mt-5 container border d-flex align-items-center justify-content-evenly"
         style={{
@@ -182,7 +183,7 @@ const Home = () => {
           backgroundColor: "#172755",
         }}
       >
-        <ul className="test-site-container  w-100 d-flex align-items-center justify-content-evenly">
+        <ul className="test-site-container w-100 d-flex align-items-center justify-content-evenly flex-wrap">
           <li>
             <img src={iconB} alt="" width="35px" className="pe-2" />
             <a
@@ -227,7 +228,6 @@ const Home = () => {
               browserleaks.com
             </a>
           </li>
-
           <li>
             <img src={iconA} alt="" width="30px" className="pe-2" />
             <a
@@ -241,60 +241,79 @@ const Home = () => {
           </li>
         </ul>
       </div>
+
       <div className="container">
-        <div className="row pt-5">
-          <div className="col-md-6 col-sx-12">
-            <div className="overflow-hidden object-fit">
-              <img src={why3am} alt="Web3 Automation" className="w-100 h-100" />
-            </div>
-          </div>
-          <div className="col-md-6 col-sx-12 ps-5">
-            <div className="overflow-hidden pt-5 mt-5 text-start">
-              <h1
-                style={{
-                  fontSize: "50px",
-                  fontWeight: "600",
-                  color: "#172755",
-                }}
-              >
-                Tại sao nên chọn 3AM
-              </h1>
-              <p>
-                <img src={iconcheck} alt="" className="pe-2" />
-                Tạo và dễ dàng quản lý không giới profile an toàn trên một máy
-                tính duy nhất
-              </p>
-              <p>
-                <img src={iconcheck} alt="" className="pe-2" />
-                Ẩn danh hoàn toàn với database fingerprint khổng lồ, hỗ trợ cả
-                nhân trình duyệt Chromium và Firefox
-              </p>
-              <p>
-                <img src={iconcheck} alt="" className="pe-2" />
-                Hỗ trợ no-code automation giúp người dùng tự tạo các tác vụ tự
-                động hóa đa luồng, tối ưu hóa thời gian và chi phí nhân sự
-              </p>
-              <p>
-                <img src={iconcheck} alt="" className="pe-2" />
-                Người dùng toàn quyền và độc lập trong việc lưu trữ profile
-              </p>
-              <p>
-                <img src={iconcheck} alt="" className="pe-2" />
-                License vĩnh viễn, giá rẻ nhất thị trường với hơn 20 ngàn thiết
-                bị đang sử dụng tính từ năm 2021
-              </p>
-            </div>
-          </div>
+  <div className="row pt-5">
+    <div className="col-md-5">
+      <div className="overflow-hidden">
+        <img src={why3am} alt="Web3 Automation" className="w-100 h-auto" />
+      </div>
+    </div>
+    <div className="col-md-7  ps-5 pt-5">
+      <div className="text-start">
+        <h1
+          style={{
+            fontSize: "50px",
+            fontWeight: "600",
+            color: "#172755",
+          }}
+        >
+          Tại sao nên chọn 3AM
+        </h1>
+        <p>
+          <img src={iconcheck} alt="" className="pe-2" />
+          Tạo và dễ dàng quản lý không giới profile an toàn trên một máy
+          tính duy nhất
+        </p>
+        <p>
+          <img src={iconcheck} alt="" className="pe-2" />
+          Ẩn danh hoàn toàn với database fingerprint khổng lồ, hỗ trợ cả
+          nhân trình duyệt Chromium và Firefox
+        </p>
+        <p>
+          <img src={iconcheck} alt="" className="pe-2" />
+          Hỗ trợ no-code automation giúp người dùng tự tạo các tác vụ tự
+          động hóa đa luồng, tối ưu hóa thời gian và chi phí nhân sự
+        </p>
+        <p>
+          <img src={iconcheck} alt="" className="pe-2" />
+          Người dùng toàn quyền và độc lập trong việc lưu trữ profile
+        </p>
+        <p>
+          <img src={iconcheck} alt="" className="pe-2" />
+          License vĩnh viễn, giá rẻ nhất thị trường với hơn 20 ngàn thiết
+          bị đang sử dụng tính từ năm 2021
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+      <div className="container">
+        <div className="mt-5 text-start">
+          <h2 style={{
+            color: '#172755',
+            fontSize: '56px',
+            fontWeight: 700,
+            lineHeight: '61.6px',
+            textAlign: 'center'
+          }}>
+            Hướng dẫn sử dụng
+          </h2>
+        </div>
+        <div>
+          <Video />
         </div>
       </div>
+
       <div className="container mt-5 chu">
-        <h2 style={{ color: "#24A1DE" }}>Bảng giá</h2>
+        <h2 style={{ color: "#24A1DE", fontWeight: "600" }}>Bảng giá</h2>
         <div className="mt-3 container-fluid">
           <p>
             Lưu ý: tài khoản chính chủ có thể reset hoặc đổi License khi muốn
             đổi PC sử dụng / PM lên page facebook để đăng kí dùng thử 7 ngày
           </p>
-          <div className=" row mt-5">
+          <div className="row mt-5">
             {/* Gói Solo */}
             <div className="col-md-4 col-sm-12">
               <div className="col-inner">
@@ -340,7 +359,7 @@ const Home = () => {
                     <div className="w-100 text-center ">
                       <a
                         href="#"
-                        className="text-decoration-none "
+                        className="text-decoration-none"
                         style={{ color: "gray" }}
                       >
                         Learn More
@@ -457,20 +476,13 @@ const Home = () => {
                       >
                         Learn More
                       </a>
+               
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="container">
-        <div className="mt-5 text-start">
-          <h2 style={{ color: "#24A1DE" }}>Hướng dẫn sử dụng</h2>
-        </div>
-        <div>
-          <Video />
         </div>
       </div>
     </div>
