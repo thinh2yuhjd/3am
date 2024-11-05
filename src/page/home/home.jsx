b1831340b36d010bff3ef3076204ba9d6b4e4908
chinhhoangtc <161810260+chinhhoangtc@users.noreply.github.com>
2024-11-05 17:56:56 +0700

    cmt
    cmt

--- src/page/home/home.jsx
+++ src/page/home/home.jsx
@@ -21,14 +21,21 @@ const Home = () => {
     navigate("/login");
   };
   const [isOpenSearch, setIsOpenSearch] = useState(false);
+
+  //menu
   const [open, setOpen] = useState(false);
+  ///
+
   const dropdownRef = useRef(null);
 
   const handleOpen = () => {
     setIsOpenSearch((prev) => !prev);
   };
 
-  const toggleDropdown = () => setOpen((prev) => !prev);
+  const toggleDropdown = () => {
+    setOpen((prev) => !prev);
+    console.log("ok");
+  };
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -44,137 +51,64 @@ const Home = () => {
   }, []);
 
   return (
-    <div style={{ backgroundColor: "#F4F6F8" }}>
+    <div className="" style={{ backgroundColor: "#F4F6F8" }}>
       <div className="am-header">
-        <img src={background} width={'100%'} />
-        <div className="am-header-search">
-          <div
-            className="border rounded-pill"
-            style={{ width: "100%", backgroundColor: "#FFFFFF" }}
-          >
-            <div className="row" style={{ height: "64px" }}>
-              <div className="col-3 my-auto text-start">
-                <img src={logo} alt="Logo" className="ps-3" />
-              </div>
-              <div className="col-6 my-auto">
-                <ul className="d-flex justify-content-around flex-wrap">
-                  <li className="nav-item">
-                    <a className="nav-link active color_nav" href="#">
-                      Trang Chủ
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link active color_nav" href="#">
-                      Bảng Giá
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/doc" className="nav-link active color_nav">
-                      Tài Liệu
-                    </Link>
-                  </li>
-                  <li className="nav-item dropdown" ref={dropdownRef}>
-                    <a
-                      className="nav-link dropdown-toggle color_nav"
-                      href="#"
-                      id="navbarDropdownMenuLink"
-                      role="button"
-                      onClick={toggleDropdown}
-                    >
-                      Mua Phần Mềm
-                    </a>
-                    {open && (
-                      <ul
-                        className="dropdown-menu"
-                        style={{
-                          position: "absolute",
-                          top: "100%",
-                          left: 0,
-                        }}
-                      >
-                        <li>
-                          <a className="dropdown-item color_nav" href="#">
-                            Mua qua facebook
-                          </a>
-                        </li>
-                        <li className="dropdown-item color_nav">
-                          <a href="" className="text-decoration-none color_nav">
-                            Mua online
-                          </a>
-                        </li>
-                      </ul>
-                    )}
-                  </li>
-                </ul>
-              </div>
-              <div className="col-3 my-auto d-flex justify-content-end pe-4">
-                <div onClick={handleOpen}>
-                  <img src={search_icon} alt="search" className="pe-3" />
-                </div>
-                <button
-                  className="button"
-                  style={{ width: "115px", backgroundColor: "#1C252E" }}
-                  onClick={onhangdleLogin}
-                >
-                  <span style={{ color: "white" }}>Đăng ký</span>
-                </button>
-              </div>
+        <img src={background} width={"100%"} />
+
+        <div className=" am-header-search">
+          <div className="row position-relative border d-flex d-flex justify-content-between">
+            <div
+              className="d-block d-md-none col-md-2 border"
+              onClick={toggleDropdown}
+            >
+              <button>button</button>
             </div>
-          </div>
-          {isOpenSearch && (
-            <div className="search-popup-overlay" onClick={handleOpen}>
-              <div
-                className="search-popup-content font-border"
-                onClick={(e) => e.stopPropagation()}
-              >
-                <input
-                  type="text"
-                  placeholder="Tìm kiếm..."
-                  className="form-controls outline-none"
-                  style={{ outline: 'none' }}
-                />
-                <img src={button_search} alt="" />
-              </div>
+
+            <div className="col-md-2 border" style={{ color: "white" }}>
+              logo
             </div>
-          )}
-        </div>
-        <div className="am-header-content mt-1">
-          <div className="container text-start">
-            <div className="col-md-9 col-lg-6 col-sm-12" style={{ color: "white" }}>
-              <h1 className="fs-1 fs-md-2 fs-lg-3 text-white" style={{ fontWeight: "600" }}>
-                Web3 Automation Multiple
-              </h1>
-              <p className="last-updated pt-3">
-                Giải pháp trình duyệt anti-detect và quản lý nhiều tài khoản
-                trên một máy tính giúp bảo vệ danh tính, vận hành an toàn và
-                hiệu quả cho các hoạt động tiếp thị và quản lý tài khoản.
-              </p>
-              
-<div className="">
-  <button
-    className="button d-none d-lg-flex align-items-center justify-content-evenly p-2" // Hiện trên máy tính lớn, ẩn trên thiết bị nhỏ hơn
-    style={{ width: "250px", backgroundColor: "#24A1DE" }}
-  >
-    <span className="icon"></span>
-    <a
-      href="#"
-      className="text-decoration-none"
-      style={{ color: "white" }}
-    >
-      Tải xuống cho Windows
-    </a>
-  </button>
-</div>
+
+            <div
+              className={`border col-md-4 position-absolute custom-menu-az ${
+                open ? "momenu" : "dongmenu"
+              }`}
+            >
+              <ul className="border h-100">
+                <li style={{listStyle:'none'}}>
+                  <a className="" href="">home</a>
+                </li>
+                <li>
+                  <a href="">Tai nguyen</a>
+                </li>
+                <li>
+                  <a href="">Tai lieu</a>
+                </li>
+                <li>
+                  <a href="">Abc</a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="d-flex border col-md-4">
+              <div className="">
+                <button>search</button>
+              </div>
+              <div className="">
+                <button>Đăng ký</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="">
-  <h1 className="custom-title" style={{ fontWeight: "600", color: "#172755" }}>
-    Được hơn 20.000 người dùng tin tưởng.
-  </h1>
-</div>
+      <div className="my-5 pb-1">
+        <h1
+          className="custom-title"
+          style={{ fontWeight: "600", color: "#172755" }}
+        >
+          Được hơn 20.000 người dùng tin tưởng.
+        </h1>
+      </div>
       <div
         className="mt-5 container border d-flex align-items-center justify-content-evenly"
         style={{
@@ -243,61 +177,67 @@ const Home = () => {
       </div>
 
       <div className="container">
-  <div className="row pt-5">
-    <div className="col-md-5">
-      <div className="overflow-hidden">
-        <img src={why3am} alt="Web3 Automation" className="w-100 h-auto" />
+        <div className="row pt-5">
+          <div className="col-md-5 col-sx-12">
+            <div className="overflow-hidden">
+              <img
+                src={why3am}
+                alt="Web3 Automation"
+                className="w-100 h-auto"
+              />
+            </div>
+          </div>
+          <div className="col-md-7 col-sx-12  ps-5 pt-5">
+            <div className="text-start">
+              <h1
+                style={{
+                  fontSize: "50px",
+                  fontWeight: "600",
+                  color: "#172755",
+                }}
+              >
+                Tại sao nên chọn 3AM
+              </h1>
+              <p>
+                <img src={iconcheck} alt="" className="pe-2" />
+                Tạo và dễ dàng quản lý không giới profile an toàn trên một máy
+                tính duy nhất
+              </p>
+              <p>
+                <img src={iconcheck} alt="" className="pe-2" />
+                Ẩn danh hoàn toàn với database fingerprint khổng lồ, hỗ trợ cả
+                nhân trình duyệt Chromium và Firefox
+              </p>
+              <p>
+                <img src={iconcheck} alt="" className="pe-2" />
+                Hỗ trợ no-code automation giúp người dùng tự tạo các tác vụ tự
+                động hóa đa luồng, tối ưu hóa thời gian và chi phí nhân sự
+              </p>
+              <p>
+                <img src={iconcheck} alt="" className="pe-2" />
+                Người dùng toàn quyền và độc lập trong việc lưu trữ profile
+              </p>
+              <p>
+                <img src={iconcheck} alt="" className="pe-2" />
+                License vĩnh viễn, giá rẻ nhất thị trường với hơn 20 ngàn thiết
+                bị đang sử dụng tính từ năm 2021
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div className="col-md-7  ps-5 pt-5">
-      <div className="text-start">
-        <h1
-          style={{
-            fontSize: "50px",
-            fontWeight: "600",
-            color: "#172755",
-          }}
-        >
-          Tại sao nên chọn 3AM
-        </h1>
-        <p>
-          <img src={iconcheck} alt="" className="pe-2" />
-          Tạo và dễ dàng quản lý không giới profile an toàn trên một máy
-          tính duy nhất
-        </p>
-        <p>
-          <img src={iconcheck} alt="" className="pe-2" />
-          Ẩn danh hoàn toàn với database fingerprint khổng lồ, hỗ trợ cả
-          nhân trình duyệt Chromium và Firefox
-        </p>
-        <p>
-          <img src={iconcheck} alt="" className="pe-2" />
-          Hỗ trợ no-code automation giúp người dùng tự tạo các tác vụ tự
-          động hóa đa luồng, tối ưu hóa thời gian và chi phí nhân sự
-        </p>
-        <p>
-          <img src={iconcheck} alt="" className="pe-2" />
-          Người dùng toàn quyền và độc lập trong việc lưu trữ profile
-        </p>
-        <p>
-          <img src={iconcheck} alt="" className="pe-2" />
-          License vĩnh viễn, giá rẻ nhất thị trường với hơn 20 ngàn thiết
-          bị đang sử dụng tính từ năm 2021
-        </p>
-      </div>
-    </div>
-  </div>
-</div>
 
       <div className="container">
         <div className="mt-5 text-start">
-          <h2 style={{
-            color: '#172755',
-            fontSize: '56px',
-            fontWeight: 700,
-            lineHeight: '61.6px',
-            textAlign: 'center'
-          }}>
+          <h2
+            style={{
+              color: "#172755",
+              fontSize: "56px",
+              fontWeight: 700,
+              lineHeight: "61.6px",
+              textAlign: "center",
+            }}
+          >
             Hướng dẫn sử dụng
           </h2>
         </div>
@@ -476,7 +416,6 @@ const Home = () => {
                       >
                         Learn More
                       </a>
-               
                     </div>
                   </div>
                 </div>
